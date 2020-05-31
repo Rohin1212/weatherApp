45cf81b420c772b0a75c05b996adf99abe731f7b
Rohin Chopra <58929990+Rohin1212@users.noreply.github.com>
2020-05-31 12:09:29 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 import json
-import jsonify
 import urllib.request
-import pprint
 from flask import Flask, render_template, request, redirect, url_for
+from datetime import date
 
 app = Flask(__name__)
-
 def kelv_to_celc(temp):
     return round(temp - 273.15,1)
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():   
+@app.route('/')
+def index():
     return render_template('index.html')
-
-    
-    
     
 @app.route('/display', methods = ['POST'])  
 def display(): 
@@ -22,18 +17,17 @@ def display():
         city = request.form['city']
         country = request.form['country']
         weather_api = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid=8b1f554d266a4addbfb19f2905f74e17"
-        print(weather_api)
         with urllib.request.urlopen(weather_api) as weather_url:
             weather_data = json.loads(weather_url.read().decode())
-            pprint.pprint((weather_data))
             final_weather_data = {}
-            final_weather_data['city'] = city
-            final_weather_data['country'] = country
+            final_weather_data['city'] = city.title()
+            final_weather_data['country'] = country.title()
             final_weather_data['temp'] = kelv_to_celc(weather_data['main']['temp'])
             final_weather_data['temp_min'] = kelv_to_celc(weather_data['main']['temp_min'])
             final_weather_data['temp_max'] = kelv_to_celc(weather_data['main']['temp_max'])
-            print(final_weather_data)
             final_weather_data['descp'] = weather_data['weather'][0]['main']
+            my_date = date.today()
+            final_weather_data['day'] = my_date.strftime("%B %d %Y")
         return render_template('index.html', final_weather_data = final_weather_data)
 
 if(__name__ == '__main__'):
